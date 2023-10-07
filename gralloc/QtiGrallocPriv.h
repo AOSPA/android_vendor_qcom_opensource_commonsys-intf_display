e3ccc7a84ae3752902baf93694ecb8012e7ecb71
Jake Weinstein <jake@aospa.co>
2023-10-07 09:18:22 +0900

    display: Make custom content metadata region opt-out
    Partially revert gralloc struct change
    
    Gralloc handle struct changes break blobs that use the previous struct
    
    Default GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE to be enabled,
    then disable for legacy targets.
    
    This partially reverts the following commit:
    34fef9c4f6e22d97c52ff60ccd93fc404ea6a4cb
    
    Co-Authored-By: Alessandro Astone <ales.astone@gmail.com>
    
    Change-Id: I9c9871d14052401a5169cefc5bac85b21bb7b58d
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- gralloc/QtiGrallocPriv.h
+++ gralloc/QtiGrallocPriv.h
@@ -150,7 +150,9 @@ struct private_handle_t : public native_handle_t {
   uint64_t base_metadata;
   uint64_t gpuaddr;
   unsigned int reserved_size;
+#ifndef GRALLOC_HANDLE_HAS_NO_CUSTOM_CONTENT_MD_RESERVED_SIZE
   unsigned int custom_content_md_reserved_size;
+#endif
   static const int kNumFds = 2;
   static const int kMagic = 'gmsm';
   unsigned int linear_size;
@@ -181,9 +183,11 @@ struct private_handle_t : public native_handle_t {
         base(0),
         base_metadata(0),
         gpuaddr(0),
-        reserved_size(0),
-        custom_content_md_reserved_size(0),
-        linear_size(0),
+        reserved_size(0)
+#ifndef GRALLOC_HANDLE_HAS_NO_CUSTOM_CONTENT_MD_RESERVED_SIZE
+        ,custom_content_md_reserved_size(0)
+#endif
+        ,linear_size(0),
         ubwcp_format(format) {
     version = static_cast<int>(sizeof(native_handle));
     numInts = NumInts();
@@ -218,11 +222,10 @@ struct private_handle_t : public native_handle_t {
   static void Dump(const private_handle_t *hnd) {
     ALOGD("handle id:%" PRIu64
           " wxh:%dx%d uwxuh:%dx%d size: %d fd:%d fd_meta:%d flags:0x%x "
-          "usage:0x%" PRIx64 "  format:0x%x layer_count: %d reserved_size = %d "
-          "custom_content_md_reserved_size = %u",
+          "usage:0x%" PRIx64 "  format:0x%x layer_count: %d reserved_size = %d",
           hnd->id, hnd->width, hnd->height, hnd->unaligned_width, hnd->unaligned_height, hnd->size,
           hnd->fd, hnd->fd_metadata, hnd->flags, hnd->usage, hnd->format, hnd->layer_count,
-          hnd->reserved_size, hnd->custom_content_md_reserved_size);
+          hnd->reserved_size);
   }
 };
 #pragma pack(pop)
